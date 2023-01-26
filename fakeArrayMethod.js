744ddaac381c4ed2040bf85866eb809740f72a53
rocioRichi <paragit@gmail.com>
2023-01-26 19:58:54 +0100

    add include and index

--- fakeArrayMethod.js
+++ fakeArrayMethod.js
@@ -11,3 +11,21 @@ export const fakePush = (Array, something) => {
   Array[longMeasure(Array)] = something;
   return Array;
 };
+
+export const fakeInclude = (Array, something) => {
+  for (let i = 0; i < longMeasure(Array); i++) {
+    if (Array[i] === something) {
+      return true;
+    }
+  }
+  return false;
+};
+
+export const fakeIndexOf = (Array, something) => {
+  for (let i = 0; i < longMeasure(Array); i++) {
+    if (Array[i] === something) {
+      return i;
+    }
+  }
+  return -1;
+};
